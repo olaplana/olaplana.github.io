4132ec2ea38028b2d3a9cfecd090c3f49c378200
olaplana <olaplana@iespompeufabra.cat>
2026-09-23 14:10:57 +0200

    Create sketch.js

---
+++ cara1/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(74, 88, 176);
+  ellipse(300,300,200,250)
+  ellipse(260,270,60,30)
+  ellipse(340,270,60,30)
+  arc(300,350,100,40,0,PI)
+  triangle(300,400,300,300,250,350)
+  
+}
